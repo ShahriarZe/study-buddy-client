9aae77345d6d3ef457b1fd9398a82e3e98bea4e1
Shahriar Ahmmed <shahriarzemam28@gmail.com>
2023-11-04 22:46:39 +0600

    footer section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom"
 import NavBar from "./Components/NavBar/NavBar"
+import Footer from "./Components/Footer/Footer"
 
 function App() {
   return (
     <div>
       <NavBar></NavBar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   )
 }
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,33 @@
+import study from '../../assets/study.png'
+const Footer = () => {
+    return (
+        <footer className="footer p-10  text-base-content">
+  <aside>
+    <img className='w-4/6' src={study} alt="" />
+    <p>Online Group Study Platform.<br/>Collecting Learners since 2010</p>
+  </aside> 
+  <nav>
+    <header className="footer-title">Services</header> 
+    <a className="link link-hover">Branding</a> 
+    <a className="link link-hover">Design</a> 
+    <a className="link link-hover">Marketing</a> 
+    <a className="link link-hover">Advertisement</a>
+  </nav> 
+  <nav>
+    <header className="footer-title">Company</header> 
+    <a className="link link-hover">About us</a> 
+    <a className="link link-hover">Contact</a> 
+    <a className="link link-hover">Jobs</a> 
+    <a className="link link-hover">Press kit</a>
+  </nav> 
+  <nav>
+    <header className="footer-title">Legal</header> 
+    <a className="link link-hover">Terms of use</a> 
+    <a className="link link-hover">Privacy policy</a> 
+    <a className="link link-hover">Cookie policy</a>
+  </nav>
+</footer>
+    );
+};
+
+export default Footer;

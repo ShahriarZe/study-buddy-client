32a6c8b54e92abda81848545f724a1dd12468a58
Shahriar Ahmmed <shahriarzemam28@gmail.com>
2023-11-06 22:08:50 +0600

    Delete method created for assignment

--- src/Components/AllAssignments/AllAssignments.jsx
+++ src/Components/AllAssignments/AllAssignments.jsx
@@ -60,7 +60,7 @@ const AllAssignments = () => {
             <div className="mt-4 flex justify-center">
                 <button
                     onClick={handlePrevPage}
-                    className="mx-2 py-2 px-4 rounded mt-4 bg-gray-300"
+                    className="mx-2 py-2 px-4 rounded mt-4 btn btn-outline btn-accent"
                     disabled={currentPage === 1}
                 >
                     Prev
@@ -69,7 +69,7 @@ const AllAssignments = () => {
                     <button
                         key={pageNumber}
                         onClick={() => handlePageChange(pageNumber)}
-                        className={`mx-2 py-2 px-4 rounded mt-4 ${
+                        className={`mx-2 py-2 px-4 rounded-full mt-4 ${
                             currentPage === pageNumber ? "bg-accent text-white" : "bg-gray-300"
                         }`}
                     >
@@ -78,7 +78,7 @@ const AllAssignments = () => {
                 ))}
                 <button
                     onClick={handleNextPage}
-                    className="mx-2 py-2 px-4 rounded mt-4 bg-gray-300"
+                    className="mx-2 py-2 px-4 rounded mt-4 btn btn-outline btn-accent"
                     disabled={currentPage === totalPages}
                 >
                     Next
@@ -89,56 +89,3 @@ const AllAssignments = () => {
 };
 
 export default AllAssignments;
-
-
-
-
-
-// Prev Code 
-
-// import { useLoaderData } from "react-router-dom";
-// import AssignmentCard from "./AssignmentCard";
-// import { useState } from "react";
-
-
-// const  assignments  = useLoaderData()
-
-// const [filter, setFilter] = useState("all");
-
-// const filteredAssignments = assignments.filter(assignment => {
-//     if (filter === "all") {
-//         return true; 
-//     } else {
-//         return assignment.difficulty === filter;
-//     }
-// });
-
-
-// return (
-//     <div className="min-h-screen  lg:p-40">
-//         <div className="mb-4 flex justify-center border border-accent max-w-xl mx-auto p-2 text-xl font-semibold rounded-xl">
-//             <label htmlFor="filter" className="mr-2">Filter by Difficulty:</label>
-//             <select
-//                 id="filter"
-//                 value={filter}
-//                 onChange={e => setFilter(e.target.value)}
-//             >
-//                 <option value="all">All</option>
-//                 <option value="easy">Easy</option>
-//                 <option value="medium">Medium</option>
-//                 <option value="hard">Hard</option>
-//             </select>
-//         </div>
-//         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 max-w-7xl mx-auto gap-5">
-//             {
-//                 filteredAssignments.map(assignment => <AssignmentCard
-//                     key={assignment._id}
-//                     assignment={assignment}
-//                 ></AssignmentCard>)
-//             }
-//         </div>
-//     </div>
-// );
-// };
-
-// export default AllAssignments;
--- src/Components/ViewAssignment/ViewAssignment.jsx
+++ src/Components/ViewAssignment/ViewAssignment.jsx
@@ -1,33 +1,78 @@
-import { useLoaderData, useParams } from "react-router-dom";
+import { useLoaderData, useLocation, useNavigate, useParams } from "react-router-dom";
 import { motion } from "framer-motion";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import Swal from "sweetalert2";
+import { AuthContext } from "../../Providers/AuthProvider";
 
 const ViewAssignment = () => {
-    const details = useLoaderData()
-    const {id }= useParams()
-    const viewDetails = details.find(item => item._id == id)
-    const [rotate,setRotate]=useState(false)
-    return (
-        <div className="max-w-5xl mx-auto min-h-screen mt-20 md:mt-20 lg:mt-0 lg:p-40">
-               <motion.div animate={{rotate : rotate ? 360 : 0}} 
-               onClick={()=>{setRotate(!rotate)}} className="card bg-base-100 shadow-xl border-accent border">
-  <figure className="px-10 pt-10">
-    <img src={viewDetails.photo} alt="Shoes" className="rounded-xl" />
-  </figure>
-  <div className="card-body text-xl">
-    <h2 className="card-title text-2xl">{viewDetails.tittle}</h2>
-    <p>Mark : {viewDetails.mark}</p>
-    <p>Difficulty : {viewDetails.difficulty}</p>
-    <p>Due : {viewDetails.date}</p>
-    <p>{viewDetails.description}</p>
-    <div className="card-actions flex justify-center">
-      <button className="btn btn-accent btn-outline">Take Assignment</button>
-      <button className="btn btn-accent btn-outline">DELETE</button>
-    </div>
-  </div>
-</motion.div>
+  const location = useLocation()
+  const navigate = useNavigate()
+  const { user } = useContext(AuthContext)
+  const details = useLoaderData()
+  const { id } = useParams()
+  const viewDetails = details.find(item => item._id == id)
+  const [rotate, setRotate] = useState(false)
+  const { _id } = viewDetails
+
+  const handleDelete = _id => {
+    console.log(_id);
+    if (user.email === viewDetails.userEmail) {
+      Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          fetch(`http://localhost:5000/assignments/${_id}`, {
+            method: 'DELETE'
+          })
+            .then(res => res.json())
+            .then(data => {
+              console.log(data);
+              if (data.deletedCount > 0) {
+                Swal.fire(
+                  'Deleted!',
+                  'Assignment has been Deleted.',
+                  'success'
+                )
+              }navigate(location?.state ? location.state : '/')
+            })
+        }
+      })
+    } else {
+      return Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'You are not Creator!',
+      });
+    }
+
+  }
+  return (
+    <div className="max-w-5xl mx-auto min-h-screen mt-20 md:mt-20 lg:mt-0 lg:p-40">
+      <motion.div animate={{ rotate: rotate ? 360 : 0 }}
+        onClick={() => { setRotate(!rotate) }} className="card bg-base-100 shadow-xl border-accent border">
+        <figure className="px-10 pt-10">
+          <img src={viewDetails.photo} alt="Shoes" className="rounded-xl" />
+        </figure>
+        <div className="card-body text-xl">
+          <h2 className="card-title text-2xl">{viewDetails.tittle}</h2>
+          <p>Mark : {viewDetails.mark}</p>
+          <p>Difficulty : {viewDetails.difficulty}</p>
+          <p>Due : {viewDetails.date}</p>
+          <p>{viewDetails.description}</p>
+          <div className="card-actions flex justify-center">
+            <button className="btn btn-accent btn-outline">Take Assignment</button>
+            <button onClick={() => handleDelete(_id)} className="btn btn-accent btn-outline">DELETE</button>
+          </div>
         </div>
-    );
+      </motion.div>
+    </div>
+  );
 };
 
 export default ViewAssignment;
